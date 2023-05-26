a92ea0ee88593883820d40af9a06fd7a4834579e
Sorta L⨀SŦ <77138706+v1s1t0r999@users.noreply.github.com>
2023-05-26 14:03:02 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
@@ -10,5 +11,7 @@ def index():
 def run(debug=False):
 	app.run("0.0.0.0", debug=debug)
 
+	
+	
 if __name__=="__main__":
 	run()
