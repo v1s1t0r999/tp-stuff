23ad6ba1ae3b9f1ac865b22fcfe2c5e8fe37f3cc
Sorta L⨀SŦ <77138706+v1s1t0r999@users.noreply.github.com>
2023-06-16 19:41:06 +0530

    oldy?

--- src/database.py
+++ src/database.py
@@ -5,17 +5,17 @@ import ShitDB
 
 WOTD = "life"
 
-db = ShitDB.DB(os.getenv("github_token"), "tp-stuff", ("v1s1t0r999", os.getenv("mail")), branch="database")
+db = ShitDB.DB(os.getenv("github_token"), "tp-stuff", ("v1s1t0r999", os.getenv("mail")), branch="open-threads")
 
 
 def read(roomname:str = None):
-    rooms = db.load_remote_data("rooms.json", eval_output=True, branch="database")
+    rooms = db.load_remote_data("rooms.json", eval_output=True)
     if roomname:
         try:
             return rooms[roomname]
         except KeyError:
             add(roomname)
-            new_rooms = db.load_remote_data("rooms.json", eval_output=True, branch="database")
+            new_rooms = db.load_remote_data("rooms.json", eval_output=True)
             return new_rooms[roomname]
     return rooms
 
@@ -24,28 +24,28 @@ def add(roomname, data=None):
     rooms = read()
     if not data:
         con = {roomname:[{"from": "SYSTEM","content": f"NEW CHANNEL NAMED \"{roomname.upper()}\"","timestamp":"00:00:00","avatar":"https://tp-stuff.vercel.app/static/ecstasy.png"}]}
-        db.push_remote_data(con,"rooms.json", branch="database")
-        return db.load_remote_data("rooms.json", eval_output=True, branch="database")[roomname]
+        db.push_remote_data(con,"rooms.json")
+        return db.load_remote_data("rooms.json", eval_output=True)[roomname]
     
     rooms[roomname].append(data)
-    db.push_remote_data(rooms, "rooms.json", branch="database")
-    return db.load_remote_data("rooms.json", eval_output=True, branch="database")[roomname]
+    db.push_remote_data(rooms, "rooms.json")
+    return db.load_remote_data("rooms.json", eval_output=True)[roomname]
 
 
 def get_avatar(_ip):
-	con = db.load_remote_data("avatars.json", eval_output=True, branch="database")
+	con = db.load_remote_data("avatars.json", eval_output=True)
 	try:
 		return con['ip_avatars'][_ip]
 	except KeyError:
 		con['ip_avatars'].update({_ip : requests.get(f"https://loremflickr.com/json/320/320/{WOTD}").json()['file']})
-		db.push_remote_data(con, "avatars.json", branch="database")
+		db.push_remote_data(con, "avatars.json")
 		return con['ip_avatars'][_ip] # returns new complete json
 
 
 def set_avatar(_ip,av):
-    con = db.load_remote_data("avatars.json", eval_output=True, branch="database")
+    con = db.load_remote_data("avatars.json", eval_output=True)
     con['ip_avatars'].update({_ip : av})
-    db.push_remote_data(con, "avatars.json", branch="database")
+    db.push_remote_data(con, "avatars.json")
     return {_ip : av}
 
 
