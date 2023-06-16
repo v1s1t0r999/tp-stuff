6fb4a37ac0aa6579b4e293679172780e7f2e3562
Sorta L⨀SŦ <77138706+v1s1t0r999@users.noreply.github.com>
2023-06-16 19:36:35 +0530

    s

--- src/database.py
+++ src/database.py
@@ -9,13 +9,13 @@ db = ShitDB.DB(os.getenv("github_token"), "tp-stuff", ("v1s1t0r999", os.getenv("
 
 
 def read(roomname:str = None):
-    rooms = db.load_remote_data("rooms.json", eval_output=True)
+    rooms = db.load_remote_data("rooms.json", eval_output=True, branch="database")
     if roomname:
         try:
             return rooms[roomname]
         except KeyError:
             add(roomname)
-            new_rooms = db.load_remote_data("rooms.json", eval_output=True)
+            new_rooms = db.load_remote_data("rooms.json", eval_output=True, branch="database")
             return new_rooms[roomname]
     return rooms
 
@@ -25,15 +25,15 @@ def add(roomname, data=None):
     if not data:
         con = {roomname:[{"from": "SYSTEM","content": f"NEW CHANNEL NAMED \"{roomname.upper()}\"","timestamp":"00:00:00","avatar":"https://tp-stuff.vercel.app/static/ecstasy.png"}]}
         db.push_remote_data(con,"rooms.json", branch="database")
-        return db.load_remote_data("rooms.json", eval_output=True)[roomname]
+        return db.load_remote_data("rooms.json", eval_output=True, branch="database")[roomname]
     
     rooms[roomname].append(data)
     db.push_remote_data(rooms, "rooms.json", branch="database")
-    return db.load_remote_data("rooms.json", eval_output=True)[roomname]
+    return db.load_remote_data("rooms.json", eval_output=True, branch="database")[roomname]
 
 
 def get_avatar(_ip):
-	con = db.load_remote_data("avatars.json", eval_output=True)
+	con = db.load_remote_data("avatars.json", eval_output=True, branch="database")
 	try:
 		return con['ip_avatars'][_ip]
 	except KeyError:
@@ -43,7 +43,7 @@ def get_avatar(_ip):
 
 
 def set_avatar(_ip,av):
-    con = db.load_remote_data("avatars.json", eval_output=True)
+    con = db.load_remote_data("avatars.json", eval_output=True, branch="database")
     con['ip_avatars'].update({_ip : av})
     db.push_remote_data(con, "avatars.json", branch="database")
     return {_ip : av}
